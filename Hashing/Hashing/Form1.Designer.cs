5058016676b389614e9e9239b0247fbe457645ca
Jayden Spencer <jaydenspencer1@gmail.com>
2016-09-11 00:04:21 +0800

    statically put string keys and values in hash table and list key value pair

--- Hashing/Hashing/Form1.Designer.cs
+++ Hashing/Hashing/Form1.Designer.cs
@@ -28,12 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.arrayHash_btn = new System.Windows.Forms.Button();
+            this.listHash_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // arrayHash_btn
+            // 
+            this.arrayHash_btn.Location = new System.Drawing.Point(249, 12);
+            this.arrayHash_btn.Name = "arrayHash_btn";
+            this.arrayHash_btn.Size = new System.Drawing.Size(127, 23);
+            this.arrayHash_btn.TabIndex = 0;
+            this.arrayHash_btn.Text = "Array to HashTable";
+            this.arrayHash_btn.UseVisualStyleBackColor = true;
+            this.arrayHash_btn.Click += new System.EventHandler(this.arrayHash_btn_Click);
+            // 
+            // listHash_btn
+            // 
+            this.listHash_btn.Location = new System.Drawing.Point(249, 41);
+            this.listHash_btn.Name = "listHash_btn";
+            this.listHash_btn.Size = new System.Drawing.Size(127, 23);
+            this.listHash_btn.TabIndex = 1;
+            this.listHash_btn.Text = "List HashTable";
+            this.listHash_btn.UseVisualStyleBackColor = true;
+            this.listHash_btn.Click += new System.EventHandler(this.listHash_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 272);
+            this.Controls.Add(this.listHash_btn);
+            this.Controls.Add(this.arrayHash_btn);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button arrayHash_btn;
+        private System.Windows.Forms.Button listHash_btn;
     }
 }
 
--- Hashing/Hashing/Form1.cs
+++ Hashing/Hashing/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections;
 
 namespace Hashing
 {
@@ -15,6 +16,38 @@ namespace Hashing
         public Form1()
         {
             InitializeComponent();
+        }
+        String[] sArray = new String[] {"peter", "columbine", "rad", "symmetry", "octogon" };
+        Hashtable sTable = new Hashtable();
+
+        public void arrayToHashTable()
+        {
+            foreach(String item in sArray)
+            {
+                Console.WriteLine("String: {0}", item);
+            }
+
+            sTable.Add(1, sArray[0]);
+            sTable.Add(20, sArray[1]);
+            sTable.Add(15, sArray[2]);
+        }
+
+        public void listHashTable()
+        {
+            foreach(DictionaryEntry pair in sTable)
+            {
+                Console.WriteLine("Key: {0}, Value: {1}", pair.Key, pair.Value);
+            }
+        }
+
+        private void arrayHash_btn_Click(object sender, EventArgs e)
+        {
+            arrayToHashTable();
+        }
+
+        private void listHash_btn_Click(object sender, EventArgs e)
+        {
+            listHashTable();
         }
     }
 }
