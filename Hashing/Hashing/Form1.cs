93521f89905bff82e849b1ba36c2e953bf4c7a04
Jayden Spencer <jaydenspencer1@gmail.com>
2016-09-22 12:59:53 +0800

    xor hash algorithm and commenting

--- Hashing/Hashing/Form1.cs
+++ Hashing/Hashing/Form1.cs
@@ -17,29 +17,52 @@ namespace Hashing
         {
             InitializeComponent();
         }
-        String[] sArray = new String[] {"peter", "columbine", "rad", "symmetry", "octogon" };
+
+        //arry of string to hash
+        String[] sArray = new String[] {"peter", "columbine", "rad", "symmetry", "octogon", "f", "2" };
+        //storage hash table key = hash, value = string
         Hashtable sTable = new Hashtable();
 
+        //array to values to to hash table method
         public void arrayToHashTable()
         {
+            //for each loop of items in array
             foreach(String item in sArray)
             {
+                //sTable add, call xor hash to store as key and item/string as value
                 Console.WriteLine("Storing String: {0}, in hashtable", item, item.GetHashCode());
-                sTable.Add(item.GetHashCode(), item);
+                sTable.Add(XORhash(item), item);
             }
         }
 
-        public void listHashTable()
+        //list hash table method
+        public void ListHashTable()
         {
+            //for each loop through hash table
+            //dictionaryEntry gets both the key and value from the hash table
             foreach(DictionaryEntry pair in sTable)
             {
+                //log it to the console with pair's Key and Value
                 Console.WriteLine("Key: {0}, Value: {1}", pair.Key, pair.Value);
             }
         }
 
-        public override int GetHashCode()
+        //XOR hash method
+        public String XORhash(string value)
         {
-            return base.GetHashCode();
+            //hash storage to continiously fold bytes together
+            int hash = 0;
+
+            //loop through length of string
+            for(int i = 0; i < value.Length; i++)
+            {
+                //iterate through characters in array
+                char c = value[i];
+                //xor operator of character located in previous line
+                hash ^= c;
+            }
+            //return xor hashed string
+            return hash.ToString();
         }
 
         private void arrayHash_btn_Click(object sender, EventArgs e)
@@ -49,7 +72,7 @@ namespace Hashing
 
         private void listHash_btn_Click(object sender, EventArgs e)
         {
-            listHashTable();
+            ListHashTable();
         }
     }
 }
